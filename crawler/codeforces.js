4e84c33f9aa9701d80fc70eb05b66c34a32891d5
iamstarlee <13253620092@163.com>
2024-07-03 14:23:02 +0800

    update two codeforces files

--- crawler/codeforces.js
+++ crawler/codeforces.js
@@ -14,19 +14,17 @@ async function getCodeforcesInfo(username) {
   //In_a_row_for_the_last_year, In_a_row_max, Solved_for_the_last_month, credit
   try {
     let res = await axios.get(
-      `https://codeforces.com/profile/${username}`,
+      `https://codeforces.com/api/user.info?handles=${username}&checkHistoricHandles=false`,
       axiosConfig
     );
-    let $ = cheerio.load(res.data);
-    let list = $('.personal-reward-box .num');
-    result.Solved_for_all_time = $(list[0]).text();
-    result.Solved_for_the_last_year = $(list[1]).text();
-    result.In_a_row_for_the_last_year = $(list[2]).text();
-    result.In_a_row_max = $(list[3]).text();
-    let tagList = $('.personal-tag-box div .num');
-    result.Solved_for_the_last_month = $(tagList[0]).text();
-    result.credit = $(tagList[1]).text();
-    result.name = $('.personal-massage-name').text();
+    let data = res.data
+    result.Solved_for_all_time = res.country
+    result.Solved_for_the_last_year = res.firstName
+    result.In_a_row_for_the_last_year = res.maxRank
+    result.In_a_row_max = res.rating
+    result.Solved_for_the_last_month = res.contribution
+    result.credit = data.maxRating
+    result.name = data.handle
 
     for (const [key, value] of Object.entries(result)) {
       if(value.endsWith('万+')) {
